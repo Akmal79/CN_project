8e8ff76195610e18853d5b54016b2490815a2b25
Akmal79 <109073778+Akmal79@users.noreply.github.com>
2024-06-24 15:41:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,48 +8,65 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-let user = {
-  username: 'user',
-  password: 'password',
-  temp_secret: '',
-  secret: '',
-};
+let users = {};
 
 app.get('/', (req, res) => {
   res.render('login');
 });
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  if (users[username]) {
+    return res.send('User already exists. Please choose a different username.');
+  }
+
+  const secret = speakeasy.generateSecret({ length: 20 });
+  users[username] = {
+    username,
+    password,
+    secret: secret.base32,
+  };
+
+  qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
+    res.render('verify', { qrCode: data_url, secret: secret.base32, username });
+  });
+});
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  if (username === user.username && password === user.password) {
-    const secret = speakeasy.generateSecret({ length: 20 });
-    user.temp_secret = secret.base32;
-
-    qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
-      res.render('verify', { qrCode: data_url, secret: secret.base32 });
-    });
+  const user = users[username];
+  if (user && user.password === password) {
+    res.render('token', { username });
   } else {
     res.send('Invalid username or password');
   }
 });
 
 app.post('/verify', (req, res) => {
-  const { token } = req.body;
+  const { token, username } = req.body;
+  const user = users[username];
   const verified = speakeasy.totp.verify({
-    secret: user.temp_secret,
+    secret: user.secret,
     encoding: 'base32',
     token,
   });
 
   if (verified) {
-    user.secret = user.temp_secret;
-    user.temp_secret = '';
-    res.send('2FA verification successful!');
+    res.redirect('/success');
   } else {
     res.send('Invalid token, verification failed.');
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.get('/success', (req, res) => {
+  res.render('success');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
